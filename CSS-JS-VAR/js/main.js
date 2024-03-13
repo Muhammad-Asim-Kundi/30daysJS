104ade5b11197c212bb85727b93f6de33f5ef1b6
Muhammad Asim Kundi <muhammadasimkundi@gmail.com>
2024-03-13 16:50:18 +0500

    working css variable with JS

---
+++ CSS-JS-VAR/js/main.js
@@ -0,0 +1,13 @@
+// //selecting all inputs on page
+// const inputs = document.querySelectorAll('.controls input')
+
+// //function that will handle all the updates made by users
+// function updates(){
+//     const suffix = this.dataset.sizing || '';
+//     document.documentElement.style.setProperty(`--${this.name}`, this.name + suffix);
+
+// }
+
+// //sitting smurf on inputs to listen the calls and tells function updates()
+// inputs.forEach(input => input.addEventListener('change', updates));
+// inputs.forEach(input => input.addEventListener('mousemove', updates));
